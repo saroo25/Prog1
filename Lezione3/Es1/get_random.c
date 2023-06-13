cdde8c4b6f75286bc1912cc3b2b48a533c0590b5
Rosario Forte <rforte111@gmail.com>
2023-06-13 18:08:32 +0200

    added lesson 3 homeworks

---
+++ Lezione3/Es1/get_random.c
@@ -0,0 +1,7 @@
+unsigned int get_random() {
+    static unsigned int m_w = 424242;
+    static unsigned int m_z = 242424;
+    m_z = 36969 * (m_z & 65535) + (m_z >> 16);
+    m_w = 18000 * (m_w & 65535) + (m_w >> 16);
+    return (m_z << 16) + m_w;
+}
